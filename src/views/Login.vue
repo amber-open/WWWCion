40fbbc6cf9038b95fd5801381183330761507805
DESKTOP-4MIO3ST\Administrator <1258789965@qq.com>
2018-10-15 23:35:20 +0800

    change login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,40 +1,35 @@
 <template>
   <v-app id="inspire">
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm6 md4 lg3 xl3>
-            <v-card class="elevation-12">
-              <v-toolbar dark color="primary">
-                <v-flex title text-xs-center>用户登录</v-flex>
-              </v-toolbar>
-              <v-card-text>
-                <v-form v-model="valid">
-                  <v-text-field
-                    prepend-icon="person"
-                    v-model="name"
-                    :rules="nameRules"
-                    :counter="10"
-                    label="用户名"
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    prepend-icon="lock"
-                    v-model="password"
-                    :rules="passwordRules"
-                    label="密码"
-                    required
-                  ></v-text-field>
-                </v-form>
-                <v-flex text-xs-center xs12>
-                  <v-btn style="width:50%" color="primary">登&nbsp;录</v-btn>
-                </v-flex>
-              </v-card-text>
-            </v-card>
-          </v-flex>
+    <v-layout pa-4 align-center justify-center>
+      <div class="m-left"></div>
+      <v-card class="m-right elevation-12">
+        <v-layout align-center justify-center column fill-height>
+          <v-card-text class="px-5 py-2">
+            <v-flex title text-xs-center>用户登录</v-flex>
+            <v-form v-model="valid">
+              <v-text-field
+                prepend-icon="person"
+                v-model="name"
+                :rules="nameRules"
+                :counter="10"
+                label="用户名"
+                required
+              ></v-text-field>
+              <v-text-field
+                prepend-icon="lock"
+                v-model="password"
+                :rules="passwordRules"
+                label="密码"
+                required
+              ></v-text-field>
+            </v-form>
+            <v-flex text-xs-center xs12>
+              <v-btn style="width:50%" color="primary">登&nbsp;录</v-btn>
+            </v-flex>
+          </v-card-text>
         </v-layout>
-      </v-container>
-    </v-content>
+      </v-card>
+    </v-layout>
   </v-app>
 </template>
 
@@ -58,3 +53,26 @@
     }
   }
 </script>
+
+<style scoped>
+  #inspire{
+    background: #B1DEB6;
+  }
+  .m-left{
+    height: 500px;
+    width: 250px;
+    background-image: url('../assets/ubg.jpg');
+    background-position: center;
+    background-size: cover;
+  }
+  .m-right{
+    height: 500px;
+    width: 400px;
+  }
+  @media screen and (max-width: 600px) {
+
+    .m-left{
+      display: none;
+    }
+  }
+</style>
